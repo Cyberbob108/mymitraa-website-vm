b709c857c20982ad3fcac52858325e107a5afdb6
M Venkatesh <artistworld918@gmail.com>
2026-09-09 13:31:50 +0530

    Update main.jsx to include AuthProvider and InsightsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { LanguageProvider } from './Context/LanguageContext'  // ← add
+import { LanguageProvider } from './Context/LanguageContext'
+import { AuthProvider } from './Context/AuthContext'
+import { InsightsProvider } from './Context/InsightsContext'
 import App from './App.jsx'
 import './index.css'
 
@@ -10,9 +12,13 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <LanguageProvider>   {/* ← wrap */}
-          <App />
-        </LanguageProvider>  {/* ← wrap */}
+        <LanguageProvider>
+          <AuthProvider>
+            <InsightsProvider>
+              <App />
+            </InsightsProvider>
+          </AuthProvider>
+        </LanguageProvider>
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
